6e670216168ee9158fb23fa51fa7f144049d47ec
Nitesh Kumar <nitesh.nawada@gmail.com>
2017-09-13 07:05:32 +0530

    Week #3: Lab-02 Completion

--- aop/src/main/java/config/AspectsConfig.java
+++ aop/src/main/java/config/AspectsConfig.java
@@ -1,17 +1,16 @@
 package config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import rewards.internal.monitor.MonitorFactory;
 import rewards.internal.monitor.jamon.JamonMonitorFactory;
 
-//	TODO-04: Update this configuration.
-//  1. Add a class-level annotation to scan for components
-//	located in the rewards.internal.aspects package.
-//	2. Add another class-level annotation to instruct Spring 
-//	to process beans that have the @Aspect annotation.
 @Configuration
+@ComponentScan("rewards.internal.aspects")
+@EnableAspectJAutoProxy
 public class AspectsConfig {
 
 	@Bean
